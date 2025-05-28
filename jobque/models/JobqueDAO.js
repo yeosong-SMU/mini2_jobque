bcb6a36cb8aa2667cff5e85d332fa584928b1db7
ssong <9yeosong@naver.com>
2025-05-28 14:44:57 +0900

    dao 수정

--- jobque/models/JobqueDAO.js
+++ jobque/models/JobqueDAO.js
@@ -1,5 +1,6 @@
 import db from './db.js';
 
+//회원 관리
 export const createMember = async(userid, pwd, name) => {
     return db.execute('insert into jq_users (userid, pwd, name) values (?, sha2(?, 256), ?)', [userid, pwd, name]);
 };
@@ -9,10 +10,10 @@ export const loginMember = async(userid, pwd) => {
     return rows[0];
 };
 
-export const findByUserId = async (userid) => {
-    const [rows] = await db.execute('select * from jq_users where userid = ?', [userid]);
-    return rows[0];
-};
+// export const findByUserId = async (userid) => {
+//     const [rows] = await db.execute('select * from jq_users where userid = ?', [userid]);
+//     return rows[0];
+// };
 
 // export const findAll = async () => {
 //     const [rows] = await db.execute('select * from member');
@@ -28,3 +29,33 @@ export const findByUserId = async (userid) => {
 //     const sql = 'delete from member where userid = ?';
 //     return db.execute(sql, [userid]);
 // };
+
+//조회
+export const list = async(users_id) => {
+    return db.execute('select * from jq_board where users_id = ?', [users_id]);
+};
+
+export const detail = async(userid, pwd, name) => {
+    return db.execute('insert into jq_comment (userid, pwd, name) values (?, sha2(?, 256), ?)', [userid, pwd, name]);
+};
+
+//수정
+
+
+//생성
+export const createQues = async(users_id, category, ques) => {
+    return db.execute('insert into jq_board (users_id, category, ques) values (?, ?, ?)', [users_id, category, ques]);
+};
+
+export const createComment = async(users_id, board_id, comment) => {
+    return db.execute('insert into jq_comment (users_id, board_id, comment) values (?, ?, ?)', [users_id, board_id, comment]);
+};
+
+//삭제
+export const removeQues = async (board_id) => {
+    return db.execute('delete from jq_board where id = ?', [board_id]);
+};
+
+export const removeComment = async (comment_id) => {
+    return db.execute('delete from jq_comment where id = ?', [comment_id]);
+};
--- jobque/routes/index.js
+++ jobque/routes/index.js
@@ -1,15 +1,25 @@
-import {Router} from 'express';
-import { getRegister, postRegister, getLogin, postLogin, logout, list, main, update, remove } from '../controller/usersController.js';
-const router = Router();
+// import {Router} from 'express';
+// import { getRegister, postRegister, getLogin, postLogin, logout, main, update, remove } from '../controllers/usersController.js';
+// const router = Router();
 
-router.get('/register', getRegister);
-router.post('/register', postRegister);
-router.get('/login', getLogin);    //로그인 화면으로
-router.post('/login', postLogin);    //로그인 체크 처리
-router.get('/logout', logout);
-router.get('/list', list);
-router.get('/main', main);
-router.post('/update', update);
-router.post('/remove', remove);
+// router.get('/register', getRegister);
+// router.post('/register', postRegister);
+// router.get('/login', getLogin);    //로그인 화면으로
+// router.post('/login', postLogin);    //로그인 체크 처리
+// router.get('/logout', logout);
+// router.get('/list', list);
+// router.get('/main', main);
+// router.post('/update', update);
+// router.post('/remove', remove);
 
-export default router;
+// export default router;
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
