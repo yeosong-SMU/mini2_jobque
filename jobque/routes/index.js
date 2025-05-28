ac86f05c85d8e53731a6cfd5406f8d8eea52a0ce
ssong <9yeosong@naver.com>
2025-05-28 14:52:46 +0900

    충돌?해결

--- jobque/routes/index.js
+++ jobque/routes/index.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-// import {Router} from 'express';
-// import { getRegister, postRegister, getLogin, postLogin, logout, main, update, remove } from '../controllers/usersController.js';
-// const router = Router();
-
-// router.get('/register', getRegister);
-// router.post('/register', postRegister);
-// router.get('/login', getLogin);    //로그인 화면으로
-// router.post('/login', postLogin);    //로그인 체크 처리
-// router.get('/logout', logout);
-// router.get('/list', list);
-// router.get('/main', main);
-// router.post('/update', update);
-// router.post('/remove', remove);
-=======
 import {Router} from 'express';
 import { getRegister, postRegister, getLogin, postLogin, logout, list, main, update, remove } from '../controllers/usersController.js';
 const router = Router();
@@ -27,16 +12,5 @@ router.get('/list', list);
 router.get('/main', main);
 router.post('/update', update);
 router.post('/remove', remove);
->>>>>>> 6ba885c3479b26c25061527fe5c6691d1ca3d324
 
-// export default router;
-
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+export default router;
